cbea072fc87f92489e6cb38d840b6c0c30602f40
slcnx <35824750+slcnx@users.noreply.github.com>
2022-05-16 09:09:27 +0800

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 git config --local pull.rebase true
+
+# 获取邮箱
+email=$(git config --global user.email)
+if [ -z "$email" ]; then
+  while true; do
+    read -p 'enter your email: ' email
+    if [ -z "$email" ]; then
+      continue
+    else
+      git config --global user.email "$email"
+    fi
+  done
+fi
+
+# 获取用户名
+name=$(git config --global user.name)
+if [ -z "$name" ]; then
+  while true; do
+    read -p 'enter your name: ' name
+    if [ -z "$name" ]; then
+      continue
+    else
+      git config --global user.name "$name"
+    fi
+  done
+fi
+
+
+
+
 git add .
 git commit -m "笔记本-`date +%F_%T`"
 
